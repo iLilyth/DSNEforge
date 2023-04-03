2dcb6d9ffd4865af4ca4142600ca83c0aa37ca65
Marvinschs <97344410+Marvinschs@users.noreply.github.com>
2023-04-03 22:07:33 +0200

    Fix `copyChat` overriding existing click events

--- DSNEforge/src/main/java/com/lilyth/modules/features/copychat/CopyChat.java
+++ DSNEforge/src/main/java/com/lilyth/modules/features/copychat/CopyChat.java
@@ -8,6 +8,7 @@ import net.minecraftforge.client.event.ClientChatReceivedEvent;
 public class CopyChat  {
     public void copyChat(ClientChatReceivedEvent e) {
         Minecraft mc = Minecraft.getMinecraft();
+        if (e.message.getChatStyle().getChatClickEvent() != null) return;
         ChatComponentText text = new ChatComponentText(e.message.getUnformattedText());
         text.setChatStyle(e.message.getChatStyle());
         text.getChatStyle().setChatClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, e.message.getUnformattedText()));
